9b6c83889758104d6cfb0cc66d82311a8e6da1a9
Akira Chiku <akira.chiku@gmail.com>
2015-09-12 21:31:49 +0900

    Add watcher.go

--- smith/watcher.go
+++ smith/watcher.go
@@ -1,1 +1,58 @@
 package smith
+
+import (
+	"log"
+	"os"
+
+	"github.com/howeyc/fsnotify"
+)
+
+type Watcher struct {
+	watcher  *fsnotify.Watcher
+	filePath string
+}
+
+func NewWatcher(filePath string) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	file, err := NewTargetFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Event:
+				if event.IsRename() || event.IsDelete() {
+					log.Println("file closed")
+					file.Close()
+				}
+				if event.IsModify() {
+					log.Println("modified")
+					err = file.BufferedLineRead()
+					if err != nil {
+						log.Printf("%s", err)
+					}
+				}
+				if event.IsCreate() {
+					log.Println("created")
+				}
+			}
+		}
+	}()
+
+	err = watcher.Watch(filePath)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	<-done
+	return nil
+}
